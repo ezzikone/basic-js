42def3a38b3cca490f455122d07c098631f5dfe8
ezzikone <47362010+ezzikone@users.noreply.github.com>
2020-02-16 22:11:14 +0400

    feat
    recursive task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,57 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+
+        let open = 0,
+            close = 0,
+        i = [];
+
+        arr.forEach(
+            function (item) {
+                if (typeof item === 'object'){
+                    open++;
+                    close++;
+                    recursion(item);
+                }
+                i.push(item.toString());
+                i.forEach(
+                    function (letter) {
+                        if (letter === '[') {
+                            open++;
+                        }
+                        if (letter === ']') {
+                            close++;
+                        }
+                    }
+                )
+            }
+        );
+
+function recursion(array) {
+    array.forEach(
+        function (item) {
+            if (typeof item === 'object') {
+                open++;
+                close++;
+                recursion(item);
+            }
+        }
+    );
+}
+
+
+
+        if (open-close > 0 && close > 0) {
+            return close + 1;
+        }
+
+        if (open-close < 0) {
+            return open + 1;
+        }
+
+        if (open-close === 0 && open > 0) {
+            return open + 1;
+        } else {
+            return open;
+        }
     }
 };
