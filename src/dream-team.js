e9dae7ddc67486d35f64e6dff5df02f8de9eb02b
ezzikone <47362010+ezzikone@users.noreply.github.com>
2020-02-16 21:13:57 +0400

    feat
    dream-team completed

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,31 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  let abreviature = [],
+      a = [];
+
+  if(!Array.isArray(members)) {
+    return false;
+  }
+
+  members.forEach(
+      function(member) {
+        if(typeof member !== 'string') {
+          return false;
+        }
+        member = member.trimLeft();
+        abreviature.push(member.slice(0,1));
+      }
+  );
+
+  abreviature.sort();
+
+  abreviature.forEach(
+      function (letter) {
+        a.push(letter.toUpperCase());
+      }
+  );
+
+  return  a.sort().join('');
 };
+
+
